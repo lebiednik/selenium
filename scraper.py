1f2596438c1aabd62840140f8fdbee2fa9a2ae6f
lebiednik <brian.lebiednik@gmail.com>
2017-10-12 16:34:33 -0400

    using alexa top list

--- scraper.py
+++ scraper.py
@@ -1,5 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-driver = webdriver.Firefox()
-driver.get('http://cnn.com')
-driver.close()
+import alexa
+
+top = alexa.top_list(10)
+base = 'http://www.'
+for url in top:
+    driver = webdriver.Firefox()
+    driver.get(base+url[1])
+    driver.close()
